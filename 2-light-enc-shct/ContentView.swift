b940caf933f5067c0d31aa63f38e92d5a6da5ecf
andrea <andreatv12@gmail.com>
2023-01-29 17:39:19 +0000

    Initial Commit

---
+++ 2-light-enc-shct/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  2-light-enc-shct
+//
+//  Created by andrea on 29/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2-light-enc-shct/__light_enc_shctApp.swift
@@ -0,0 +1,17 @@
+//
+//  __light_enc_shctApp.swift
+//  2-light-enc-shct
+//
+//  Created by andrea on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct __light_enc_shctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
